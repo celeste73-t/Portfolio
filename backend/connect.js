eb9330e1d4da2b01efafeaeff52820d8694fc885
celgjv <celeste73310@gmail.com>
2025-03-25 11:22:40 +0100

    add MongoDB connection module for PortfolioEN database

---
+++ backend/connect.js
@@ -0,0 +1,25 @@
+const {MongoClient, ServerApiVersion} = require('mongodb')
+require('dotenv').config({path: "./config.env"})
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.ATLAS_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+let database
+let dbCible = "PortfolioEN"
+
+module.exports = {
+    connectToServer: () => {
+        database = client.db(dbCible)
+    },
+    getDb: () => {
+        return database
+    }
+}
+
+console.log("the server is running")
