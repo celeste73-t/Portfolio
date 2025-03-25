4e5c372e4366810184c597ae53344dc27396d324
celgjv <celeste73310@gmail.com>
2025-03-25 12:20:52 +0100

    Add language routes for retrieving all and individual languages

---
+++ backend/languageRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const database = require("./connect")
+const ObjectId = require("mongodb").ObjectId
+
+let languageRoutes = express.Router()
+
+// 1 - Retrieve All
+// http://localhost:3000/languages
+languageRoutes.route("/languages").get(async(request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("Language_Card").find({}).toArray()
+    if (data.length > 0) {
+        response.json(data) // return equivalent
+    }else {
+        throw new Error("Data was not found")
+    }
+})
+
+// 2 - Retrieve One
+// http://localhost:3000/languages/28374
+languageRoutes.route("/languages/:id").get(async(request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("Language_Card").findOne({_id: new ObjectId(request.params.id)})
+    if (Object.keys(data).length > 0) {
+        response.json(data) // return equivalent
+    }else {
+        throw new Error("Data was not found")
+    }
+})
