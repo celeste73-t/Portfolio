dc279c97f49548118847a96ce8a68b1eb332d166
celgjv <celeste73310@gmail.com>
2025-03-26 14:13:30 +0100

    Add main page routes for retrieving main page data

---
+++ backend/mainPageRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const database = require("./connect")
+const ObjectId = require("mongodb").ObjectId
+
+let mainPageRoutes = express.Router()
+
+// 1 - Retrieve
+// http://localhost:3000/mainpage
+mainPageRoutes.route("/mainpage").get(async(request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("Main_Page").findOne({})
+    if (Object.keys(data).length > 0) {
+        response.json(data) // return equivalent
+    }else {
+        throw new Error("Data was not found")
+    }
+})
+
+module.exports = mainPageRoutes
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const connect = require("./connect")
 const express = require("express")
 const cors = require("cors")
 const language = require("./languageRoutes")
+const mainPage = require("./mainPageRoutes")
 
 const app = express()
 const PORT = 3000
@@ -9,6 +10,7 @@ const PORT = 3000
 app.use(cors())
 app.use(express.json())
 app.use(language)
+app.use(mainPage)
 
 app.listen(PORT, () => {
     connect.connectToServer()
