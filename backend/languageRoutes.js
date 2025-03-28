a0c5168f640f743d48fc6f7e419d8b1e68103a16
celgjv <celeste73310@gmail.com>
2025-03-28 14:10:14 +0100

    Refactor Language_Card component to fetch language data dynamically and update rendering logic

--- backend/languageRoutes.js
+++ backend/languageRoutes.js
@@ -20,7 +20,7 @@ languageRoutes.route("/languages").get(async(request, response) => {
 // http://localhost:3000/languages/28374
 languageRoutes.route("/languages/:id").get(async(request, response) => {
     let db = database.getDb()
-    let data = await db.collection("Language_Card").findOne({_id: new ObjectId(request.params.id)})
+    let data = await db.collection("Language_Card").findOne({_id: request.params.id})
     if (Object.keys(data).length > 0) {
         response.json(data) // return equivalent
     }else {
--- frontend/src/Component/Language_Card.jsx
+++ frontend/src/Component/Language_Card.jsx
@@ -4,94 +4,23 @@ import { useEffect, useState } from "react";
 
 function Language_Card(props) {
 
-    switch (props.id) {
-        case 1:
-            var image = "src/assets/Java.svg";
-            var name = "Java";
-            break;
+    const [language_Card, setLanguage_Card] = useState({})
 
-        case 2:
-            var image = "src/assets/C_sharp.svg";
-            var name = "C#";
-            break;
+    let id = "langage_" + props.id
 
-        case 3:
-            var image = "src/assets/Unity.svg";
-            var name = "Unity";
-            break;
-
-        case 4:
-            var image = "src/assets/Godot.svg";
-            var name = "Godot";
-            break;
-        
-        case 5:
-            var image = "src/assets/Opengl.svg";
-            var name = "OpenGL";
-            break;
-        
-        case 6:
-            var image = "src/assets/Cuda.svg";
-            var name = "CUDA";
-            break;
-
-        case 7:
-            var image = "src/assets/HTML5.svg";
-            var name = "HTML";
-            break;
-
-        case 8:
-            var image = "src/assets/CSS3.svg";
-            var name = "CSS";
-            break;
-
-        case 9:
-            var image = "src/assets/Javascript.svg";
-            var name = "JavaScript";
-            break;
-        
-        case 10:
-            var image = "src/assets/React.svg";
-            var name = "React";
-            break;
-        
-        case 11:
-            var image = "src/assets/C.svg";
-            var name = "C";
-            break;
-
-        case 12:
-            var image = "src/assets/Python.svg";
-            var name = "Python";
-            break;
-
-        case 13:
-            var image = "src/assets/Tensorflow.svg";
-            var name = "TensorFlow";
-            break;
-
-        case 14:
-            var image = "src/assets/OpenCV.svg";
-            var name = "OpenCV";
-            break;
-        
-        case 15:
-            var image = "src/assets/Sql.svg";
-            var name = "SQL";
-            break;
-
-        default:
-            var image = "https://placehold.co/75";
-            var name = "placeholder";
-            break;
-    }
-
+    useEffect(() => {
+        async function loadLanguage_Card() {
+            let data = await getLanguage_Card(id)
+            setLanguage_Card(data)
+        }
+        loadLanguage_Card()
+    }, [])
 
 
     return(
         <Link className= {`language_card ${props.anim? 'animate animate'+(props.id): ''}`} to={"/projects"}>
-            <img src={image} class="svg-icon" alt={name} />
-            <h3>{name}</h3>
+            <img src={language_Card.image} className="svg-icon" alt={language_Card.name} />
+            <h3>{language_Card.name}</h3>
         </Link>
     );
 }
